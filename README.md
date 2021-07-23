02e97f8608cd494dfbc783ee459f4b71d3367c77
Tim Lauridsen <tla@rasmil.dk>
2021-07-23 13:24:40 +0200

    Add Save/Load support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ GTK3 CSS Prototyping applications like codepen.io for web development
 
 It will load demo.css in an code editor to the left and the Gtk:builder demo.ui will loaded to the right
 
-When **Apply CSS** is pressed, the CSS in the code editor will be applied to the demo.ui content on the right side
+When **Apply** is pressed, the CSS in the code editor will be applied to the demo.ui content on the right side
 
 
 ## How to run
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+#  Copyright (C) 2021 Tim Lauridsen < tla[at]rasmil.dk >
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to
+#  the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
 
+"""
+CSS Codepen like application for GTK
+"""
 import sys
-import gi
 import os
+import os.path
+
+import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('GtkSource', '3.0')
 from gi.repository import Gtk, Gdk, GLib, GtkSource, Gio  # type: ignore
@@ -9,6 +31,8 @@ from gi.repository import Gtk, Gdk, GLib, GtkSource, Gio  # type: ignore
 
 class App(Gtk.Application):
     __gtype_name__ = 'GtkMyApp'
+    windows_title = "Gtk3 CSS Codepen"
+    default_css = './user.css'
 
     def __init__(self):
         Gtk.Application.__init__(self, application_id="dk.rasmil.sourceview")
@@ -17,11 +41,12 @@ class App(Gtk.Application):
         self.builder.add_from_file('main.ui')
         self.demo_ui = Gtk.Builder()
         self.demo_ui.add_from_file('demo.ui')
+        self.current_css_file = self.default_css
 
     def on_activate(self, app):
         self.window = self.builder.get_object('mainwin')
         self.window.set_application(app)
-        self.window.set_title("Gtk3 CSS Codepen")
+        self.window.set_title(self.windows_title)
         self.window.set_default_size(1600, 800)
         self.provider = Gtk.CssProvider()
         # Setup left & right
@@ -34,8 +59,10 @@ class App(Gtk.Application):
         self.buffer, self.sourceview = self.setup_sourceview()
         # load file into sourceview
         self.load_file()
-        btn = self.builder.get_object('apply_css')
-        btn.connect('clicked', self.on_button_clicked)
+        # setup buttons
+        for id_name in ['apply_css', 'open', 'save']:
+            btn = self.builder.get_object(id_name)
+            btn.connect('clicked', self.on_button_clicked)
         self.left.add(self.sourceview)
         self.left.show_all()
         self.load_main_css()
@@ -49,6 +76,7 @@ class App(Gtk.Application):
         sourceview.set_insert_spaces_instead_of_tabs(True)
         sourceview.set_smart_backspace(True)
         sourceview.set_tab_width(2)
+        buffer.set_highlight_matching_brackets(False)
         lang_manager = GtkSource.LanguageManager()
         buffer.set_language(lang_manager.get_language('css'))
         style_mgr = GtkSource.StyleSchemeManager()
@@ -57,11 +85,21 @@ class App(Gtk.Application):
         buffer.set_style_scheme(style)
         return buffer, sourceview
 
-    def load_file(self):
+    def load_file(self, filename=None):
+        if not filename:
+            filename = 'demo.css'
+        print(f'Loading : {filename}')
         file = GtkSource.File()
-        file.set_location(Gio.File.new_for_path('demo.css'))
+        file.set_location(Gio.File.new_for_path(filename))
         loader = GtkSource.FileLoader.new(self.buffer, file)
         loader.load_async(GLib.PRIORITY_LOW, None, None, None, None, None)
+
+    def save_file(self, filename):
+        print(f'Saving : {filename}')
+        file = GtkSource.File()
+        file.set_location(Gio.File.new_for_path(filename))
+        loader = GtkSource.FileSaver.new(self.buffer, file)
+        loader.save_async(GLib.PRIORITY_LOW, None, None, None, None, None)
 
     def load_main_css(self):
         screen = Gdk.Screen.get_default()
@@ -94,13 +132,75 @@ class App(Gtk.Application):
                 raise e
         Gtk.StyleContext.reset_widgets(Gdk.Screen.get_default())
 
+    def file_open(self):
+        dialog = Gtk.FileChooserDialog(
+            action=Gtk.FileChooserAction.OPEN, title="Select a .CSS file")
+        buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                   Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+        dialog.add_buttons(*buttons)
+        flt = Gtk.FileFilter()
+        flt.set_name("Content Style Sheet files")
+        flt.add_pattern("*.css")
+        dialog.add_filter(flt)
+        dialog.set_current_folder('.')
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            file_path = dialog.get_filename()
+            self.load_file(file_path)
+            self.current_css_file = file_path
+            self.window.set_title(
+                f'{self.windows_title} - {os.path.basename(file_path)}')
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def file_save(self, overwrite=False):
+        dialog = Gtk.FileChooserDialog(
+            action=Gtk.FileChooserAction.SAVE, title="Save File")
+        buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                   Gtk.STOCK_SAVE, Gtk.ResponseType.OK)
+        dialog.add_buttons(*buttons)
+        flt = Gtk.FileFilter()
+        flt.set_name("Content Style Sheet files")
+        flt.add_pattern("*.css")
+        dialog.add_filter(flt)
+        dialog.set_current_folder(os.path.dirname(self.current_css_file))
+        dialog.set_filename(self.current_css_file)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            file_path = dialog.get_filename()
+            if not os.path.exists(file_path) or overwrite or self.save_overwrite(file_path):
+                self.save_file(file_path)
+                self.current_css_file = file_path
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def save_overwrite(self, file_path):
+        messagedialog = Gtk.MessageDialog(transient_for=self.window,
+                                          modal=True,
+                                          destroy_with_parent=True,
+                                          message_type=Gtk.MessageType.INFO,
+                                          buttons=Gtk.ButtonsType.YES_NO,
+                                          text=f"Do you want to overwrite ? {os.path.basename(file_path)}")
+        messagedialog.set_default_response(Gtk.ResponseType.NO)
+        response = messagedialog.run()
+        messagedialog.destroy()
+        return response == Gtk.ResponseType.YES
+
     def on_button_clicked(self, widget):
         label = widget.get_label()
         # print(f'Button {label} Pressed')
-        if label == 'Apply CSS':
+        if label == 'gtk-apply':
             txt = self.buffer.get_text(
                 self.buffer.get_start_iter(), self.buffer.get_end_iter(), False)
             self.update_css()
+        elif label == "gtk-open":
+            self.file_open()
+        elif label == "gtk-save":
+            self.file_save()
 
 
 if __name__ == '__main__':
